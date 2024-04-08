c9598a0573d98932d8f769a80b2a9b611315181a
nokamun <muneystackz36@gmail.com>
2024-04-07 21:53:59 -0400

    draft one of user interface

--- app/views.py
+++ app/views.py
@@ -1,10 +1,12 @@
-from app import app
-from flask import render_template
+#from app import app
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('layout.html')
 
 
 if __name__ == '__main__':
